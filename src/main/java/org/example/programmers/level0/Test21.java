055b304832a8da1b9fe047d8e52079f4d6423a3b
Hong Ui Ju <dmlwn902@naver.com>
2023-08-04 16:09:45 +0900

    230804 프로그래머스0_삼각형의 완성조건(1)

---
+++ src/main/java/org/example/programmers/level0/Test21.java
@@ -0,0 +1,30 @@
+package org.example.programmers.level0;
+
+import java.util.Arrays;
+
+/**
+ * 문제이름 : 삼각형의 완성조건(1)
+ *
+ * 문제설명 :
+ * 선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+ *  - 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+ * 삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항 :
+ * sides의 원소는 자연수입니다.
+ * sides의 길이는 3입니다.
+ * 1 ≤ sides의 원소 ≤ 1,000
+ *
+ * 입출력 예 :
+ * [1, 2, 3]	2
+ * [3, 6, 2]	2
+ * [199, 72, 222]	1
+ */
+import java.util.*;
+
+public class Test21 {
+    public int solution(int[] sides) {
+        Arrays.sort(sides);
+        return sides[2] < sides[0] + sides[1] ? 1 : 2;
+    }
+}

8465d8e91f2fce44da9c9ea5e2e172c631dc3052
honguniverse <dmlwn902@naver.com>
2023-08-06 15:18:41 +0900

    230806 프로그래머스0_카운트 다운

---
+++ src/main/java/org/example/programmers/level0/Test46.java
@@ -0,0 +1,34 @@
+package org.example.programmers.level0;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+
+/**
+ * 문제이름 : 카운트 다운
+ *
+ * 문제설명 :
+ * 정수 start와 end가 주어질 때, start에서 end까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항 :
+ * 0 ≤ end ≤ start ≤ 50
+ *
+ * 입출력 예 :
+ * start	end	result
+ * 10	     3	[10, 9, 8, 7, 6, 5, 4, 3]
+ */
+public class Test46 {
+    public int[] solution(int start, int end) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = start; i >=end; i--){
+            list.add(i);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] otherSolution(int start, int end) {
+        return IntStream.rangeClosed(-start, -end).map(i -> -i).toArray();
+    }
+}
